bf1007686985ef1d0b2e53149679b2b2e046af42
KOSTA <KOSTA@192.168.0.199>
2017-05-25 09:46:28 +0900

    1차 css

--- TomoRoad/src/main/java/org/kosta/tomoroad/controller/HomeController.java
+++ TomoRoad/src/main/java/org/kosta/tomoroad/controller/HomeController.java
@@ -8,9 +8,21 @@ import org.springframework.web.servlet.ModelAndView;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HomeController {	
 	@RequestMapping("home.do")
 	public ModelAndView home(){
 		return new ModelAndView("home.tiles");
 	}
+	
+	/*@RequestMapping("{viewName}.do")
+	public String showView(@PathVariable String viewName) {
+		System.out.println("@PathVariable:" + viewName);
+		return viewName + ".tiles";
+	}
+	
+	@RequestMapping("{dirName}/{viewName}.do")
+	public String showView(@PathVariable String dirName, @PathVariable String viewName) {
+		System.out.println("@PathVariable:" + dirName + "/" + viewName);
+		return dirName + "/" + viewName + ".tiles";
+	}*/
 }
--- TomoRoad/src/main/webapp/WEB-INF/views/home.jsp
+++ TomoRoad/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    <h4>HOME</h4>
+	pageEncoding="UTF-8"%>
+
+		<h4>HOME</h4>
+	
