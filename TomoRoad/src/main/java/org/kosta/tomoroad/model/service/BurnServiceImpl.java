1b6a2e78f5f5dd2ba18fa0694e1bce587aa51b94
KOSTA <KOSTA@192.168.0.110>
2017-05-26 16:51:11 +0900

    Resource에다가 name을 *Impl지정

--- TomoRoad/src/main/java/org/kosta/tomoroad/model/service/BurnServiceImpl.java
+++ TomoRoad/src/main/java/org/kosta/tomoroad/model/service/BurnServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BurnServiceImpl implements BurnService{
-	@Resource
+	@Resource(name="burnDAOImpl")
 	private BurnDAO burnDAO;	
 	
 	@Override
