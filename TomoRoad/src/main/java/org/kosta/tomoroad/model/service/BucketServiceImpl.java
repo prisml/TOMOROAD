4978e8a5f4020cfa27e2cf0404151a3179f85db3
kim moon il <kim moon il@DESKTOP-SGJMFQI>
2017-06-25 11:29:31 +0900

    주석단듯

--- TomoRoad/src/main/java/org/kosta/tomoroad/model/service/BucketServiceImpl.java
+++ TomoRoad/src/main/java/org/kosta/tomoroad/model/service/BucketServiceImpl.java
@@ -13,29 +13,24 @@ public class BucketServiceImpl implements BucketService {
 	@Resource(name="bucketDAOImpl")
 	private BucketDAO dao;
 	
-	/* (non-Javadoc)
-	 * @see org.kosta.tomoroad.model.service.BucketService#findBucket(org.kosta.tomoroad.model.vo.BucketVO)
-	 */
 	@Override
 	public int findBucket(BucketVO vo){
+		//바구니에 역이있는지 확인하는 서비스
 		return dao.findBucket(vo);
 	}
-	/* (non-Javadoc)
-	 * @see org.kosta.tomoroad.model.service.BucketService#insertBucket(org.kosta.tomoroad.model.vo.BucketVO)
-	 */
 	@Override
 	public void insertBucket(BucketVO vo){
+		//바구니에 역을 추가하는 서비스
 		dao.insertBucket(vo);
 	}
-	/* (non-Javadoc)
-	 * @see org.kosta.tomoroad.model.service.BucketService#deleteBucket(org.kosta.tomoroad.model.vo.BucketVO)
-	 */
 	@Override
 	public void deleteBucket(BucketVO vo){
+		//바구니에 담긴역을 삭제하는 서비스
 		dao.deleteBucket(vo);
 	}
 	@Override
 	public List<BucketVO> selectBucketList(String id){
+		//회원의 바구니를 불러오는 서비스
 		return dao.selectBucketList(id);
 	}
 }
--- TomoRoad/src/main/webapp/WEB-INF/views/bucket/deleteBucket_result.jsp
+++ TomoRoad/src/main/webapp/WEB-INF/views/bucket/deleteBucket_result.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <script>
-	alert("삭제 완료");
-	history.back();
+   alert("삭제 완료");
+   history.back();
 </script>
